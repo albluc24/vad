b0b589fc217cafd4aad99663600da69f802e4ed2
albluc24 <albluc24@hotmail.com>
2024-05-25 16:31:21 +0200

    transitioned to librosa and calculating the mfcc to try for clustering

--- method1.py
+++ method1.py
@@ -1,11 +1,32 @@
 import sys
+import numpy as np
+from utils import find_nonzero
 from pydub import AudioSegment as audio
-from scipy.io import wavfile
-from pysptk.sptk import swipe
+import librosa
+from pysptk.sptk import rapt as swipe
 audio_file=sys.argv[1]
 output_file=sys.argv[2]
-sound=audio.from_file(audio_file)
+stride=0.01
+sound=audio.from_file(audio_file)[:10000]
 sound.set_frame_rate(16000).export("temp.wav",format="wav")
-arr, sr=wavfile.read("temp.wav")
-result=swipe(arr, sr, 0.01*sr)
+arr, sr=librosa.load("temp.wav")
+
+framelen=int(stride*sr)
+pitch=swipe(arr, sr, framelen)
+sp=librosa.feature.mfcc(arr, sr, n_mfcc=13, hop_length=framelen, n_fft=framelen*2).T
 breakpoint()
+segments=find_nonzero(pitch)
+for n in range(len(segments)):
+    segments[n][0]=segments[n][0]*stride*1000
+    segments[n][1]=segments[n][1]*stride*1000
+newsegments=[]
+#merge segments that have a gap between them of less than 1000ms
+for n in range(len(segments)):
+    if len(newsegments)==0: newsegments.append(segments[n]); continue
+    if segments[n][0]-newsegments[-1][1]<=1000: newsegments[-1][1]=segments[n][1]
+    else: newsegments.append(segments[n])
+segments=newsegments
+out=audio.empty()
+for start, end in segments:
+    out+=sound[start:end]
+out.export(output_file,format="wav")
---
+++ utils.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+def find_nonzero(arr):
+    # Create a list with segment start and end 
+    nonzero_indices = np.nonzero(arr)[0]
+    # Initialize list to hold start and end indices of nonzero blocks
+    nonzero_blocks = []
+    
+    if nonzero_indices.size > 0:
+        # Starting index of the first nonzero block
+        start_index = nonzero_indices[0]
+
+        # Loop over the nonzero indices to find continuous blocks
+        for i in range(1, len(nonzero_indices)):
+            # Check if current index is not consecutive
+            if nonzero_indices[i] != nonzero_indices[i - 1] + 1:
+                # Record the end index of the previous block
+                end_index = nonzero_indices[i - 1]
+                # Add the start and end to the list
+                nonzero_blocks.append([start_index, end_index])
+                # Update the start index to current
+                start_index = nonzero_indices[i]
+        
+        # Add the last block
+        nonzero_blocks.append([start_index, nonzero_indices[-1]])
+    
+    return nonzero_blocks
+def wvad(file_path, frame_size=10, padding=100, aggressiveness=3, cleanup_temp=True):
+    name=file_path.split('/')[-1]
+    if exists(f'{cache_path}/{name}silences.dat'): segments=load(f'{cache_path}/{name}silences.dat')
+    else:
+        tempfiles=create_temporary_audio(file_path, [16000, 32000])
+        sound, sr=vad.read_wave(tempfiles[0])
+        vadobj=vad.webrtcvad.Vad(aggressiveness)
+        frames=vad.frame_generator(frame_size, sound, sr)
+        frames=list(frames)
+        segments=[]
+        for s,e in vad.vad_collector(sr, frame_size, padding, vadobj, frames):
+            s=int(s*1000)
+            e=int(e*1000)
+            if len(segments)==0: segments.append([s, e]); continue
+            if e-s<=1000: segments[-1][1]=e; continue
+            segments.append([s, e])
+        dump(segments, f'{cache_path}/{name}silences.dat')
+    if cleanup_temp:
+        cleanup_temporary_audio(file_path)
+        try: os.remove('tosplit.wav')
+        except: pass
+    return segments
