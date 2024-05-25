37ef1969fa05b369bccc3d9e7c857b1cfb5f8e94
albluc24 <albluc24@hotmail.com>
2024-05-25 19:50:04 +0200

    fixed 1 and adding 2(ml based)

--- method2.py
+++ method2.py
@@ -1,9 +1,10 @@
-import sys
+import sys, torch
+from s3prl import hub
 import numpy as np
 from utils import find
 from joblib import dump
 from pydub import AudioSegment as audio
-#from sklearn.mixture import GaussianMixture
+from sklearn.mixture import GaussianMixture
 #using kmeans instead of GMM
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
@@ -11,19 +12,21 @@ import librosa
 from pysptk.sptk import rapt as swipe
 audio_file=sys.argv[1]
 output_file=sys.argv[2]
+model=hub.modified_cpc()
 stride=0.01
-sound=audio.from_file(audio_file)[:30000]
+sound=audio.from_file(audio_file)[:5000]
 sound.set_frame_rate(16000).export("temp.wav", format="wav")
 arr, sr=librosa.load("temp.wav")
 framelen=int(stride*sr)
 pitch=swipe((arr*32767).astype(np.int16), sr, framelen)
-sp=librosa.feature.mfcc(arr, sr, n_mfcc=13, hop_length=framelen, n_fft=framelen*2).T
-features=np.hstack((sp, pitch.reshape(-1,1)))
+breakpoint()
+features=model(arr)
+#features=np.hstack((sp, pitch.reshape(-1,1)))
 scaler = StandardScaler()
-features_scaled = scaler.fit_transform(features)
+#features_scaled = scaler.fit_transform(features)
 gmm = GaussianMixture(n_components=2, random_state=42)
-gmm=KMeans(n_clusters=2, random_state=42)
-gmm.fit(features_scaled)
+#gmm=KMeans(n_clusters=2, random_state=42)
+gmm.fit(features)
 labels = gmm.predict(features_scaled)
 #segments=find_nonzero(pitch)
 segments=find(labels, lambda x: x==0)
--- utils.py
+++ utils.py
@@ -1,4 +1,30 @@
 import numpy as np
+def find(arr, condition):
+    # Apply the condition to the array and get indices where the condition is true
+    condition_indices = np.where(condition(arr))[0]
+    # Initialize list to hold start and end indices of continuous blocks
+    continuous_blocks = []
+    
+    if condition_indices.size > 0:
+        # Starting index of the first block where condition is true
+        start_index = condition_indices[0]
+
+        # Loop over the indices where condition is true to find continuous blocks
+        for i in range(1, len(condition_indices)):
+            # Check if current index is not consecutive
+            if condition_indices[i] != condition_indices[i - 1] + 1:
+                # Record the end index of the previous block
+                end_index = condition_indices[i - 1]
+                # Add the start and end to the list
+                continuous_blocks.append([start_index, end_index])
+                # Update the start index to current
+                start_index = condition_indices[i]
+        
+        # Add the last block
+        continuous_blocks.append([start_index, condition_indices[-1]])
+    
+    return continuous_blocks
+
 
 def find_nonzero(arr):
     # Create a list with segment start and end 
