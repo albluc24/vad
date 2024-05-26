51ebd925eb741ab985e09342f8365192ddd46a84
albluc24 <albluc24@hotmail.com>
2024-05-26 20:17:57 +0200

    2 turned out to be very similar to 1, but it works and seems to be robust to consonants, but filters out quite a bit of breaths.

--- method2.py
+++ method2.py
@@ -15,23 +15,25 @@ from soundfile import read
 from pysptk.sptk import rapt as swipe
 audio_file=sys.argv[1]
 output_file=sys.argv[2]
-model=hub.tera()
+model=hub.mel()
 stride=0.01
-sound=audio.from_file(audio_file)[:60000*5]
+sound=audio.from_file(audio_file)
 sound.set_frame_rate(16000).export("temp.wav", format="wav")
 arr, sr=read('temp.wav')
 framelen=int(stride*sr)
 pitch=swipe((arr*32767).astype(np.int16), sr, framelen)
 aud=torch.tensor(arr)
 features=model([aud.to(torch.float32)])['last_hidden_state'][0].to(torch.float64).numpy()
-pca=PCA(500)#20without scaling 30 without scaling
-features=pca.fit_transform(features)
-features=np.hstack((features[:LEN(PITCH)], pitch.reshape(-1,1)))
+breakpoint()
+features=features[:,70:]
+pca=PCA(79)#20without scaling 30 without scaling
+#features=pca.fit_transform(features)
+features=np.hstack((features[:len(pitch)], pitch.reshape(-1,1)))
 scaler = StandardScaler()
-#features_scaled = scaler.fit_transform(features)
+features_scaled = scaler.fit_transform(features)
 features=features_scaled
-gmm = GaussianMixture(n_components=2, random_state=42, verbose=1)
-#gmm=KMeans(n_clusters=2, random_state=42)
+#gmm = GaussianMixture(n_components=2, random_state=42, verbose=1, init_params='kmeans')
+gmm=KMeans(n_clusters=2, random_state=42)
 gmm.fit(features)
 labels = gmm.predict(features)
 silence=int(labels[0])
